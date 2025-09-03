9f6e8a1f228762ae004012c8c0aa4da37b907997
fravl <valentin.roettgen@gmail.com>
2025-09-03 21:19:38 +0300

    refactor: move tower to game instance

--- lib/components/play_area.dart
+++ lib/components/play_area.dart
@@ -1,11 +1,8 @@
-// lib/components/play_area.dart
 import 'dart:async';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
-import 'package:stapelmeister/components/tower_controller.dart';
-import 'package:stapelmeister/config.dart';
 import 'package:stapelmeister/stapelmeister.dart';
 
 class PlayArea extends RectangleComponent
@@ -16,20 +13,14 @@ class PlayArea extends RectangleComponent
           children: [RectangleHitbox()],
         );
 
-  late final TowerController tower;
-
   @override
   FutureOr<void> onLoad() async {
     size = Vector2(game.width, game.height);
-
-    // You can choose difficulty here:
-    tower = TowerController(level: Level.easy);
-    add(tower);
   }
 
   @override
   void onTapDown(TapDownEvent event) {
-    tower.drop();
+    game.tower.drop();
     super.onTapDown(event);
   }
 }
--- lib/stapelmeister.dart
+++ lib/stapelmeister.dart
@@ -1,21 +1,23 @@
 import 'dart:async';
-
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:stapelmeister/components/play_area.dart';
+import 'package:stapelmeister/components/tower_controller.dart';
 import 'package:stapelmeister/config.dart';
 
 class Stapelmeister extends FlameGame {
   Stapelmeister()
-    : super(
-        camera: CameraComponent.withFixedResolution(
-          width: gameWidth,
-          height: gameHeight,
-        ),
-      );
+      : super(
+          camera: CameraComponent.withFixedResolution(
+            width: gameWidth,
+            height: gameHeight,
+          ),
+        );
 
   double get width => size.x;
   double get height => size.y;
+
+  late final TowerController tower;
 
   @override
   FutureOr<void> onLoad() async {
@@ -23,6 +25,9 @@ class Stapelmeister extends FlameGame {
 
     camera.viewfinder.anchor = Anchor.topLeft;
 
-    world.add(PlayArea());
+    add(PlayArea());
+
+    tower = TowerController();
+    add(tower);
   }
 }
